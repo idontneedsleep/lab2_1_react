f5b193ed52df8a481779e8cd0fb0af08ddbeef54
Евеліна Федоренко <ipz192_feo@student.ztu.edu.ua>
2023-05-06 20:26:30 +0300

    task 3 guess number game

--- src/components/numbers.js
+++ src/components/numbers.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {isDisabled} from "@testing-library/user-event/dist/utils";
 
 export class Number extends React.Component {
     static defaultProps = {
         min : 1,
         max : 1000,
+        info : ' ',
+        attemptAmount : 10,
+        result : '',
     };
     constructor(props) {
         super(props);
@@ -11,16 +15,31 @@ export class Number extends React.Component {
             value : props.value,
             min : props.min,
             max : props.max,
+            info : props.info,
+            attemptAmount : props.attemptAmount,
             result : props.result,
         };
         this.handleNewGame = this.handleNewGame.bind(this);
         this.handleCheck = this.handleCheck.bind(this);
+        this.showInfo = this.showInfo.bind(this);
+    }
+
+    componentDidMount() {
+        document.getElementById('check').disabled = true;
+    }
+
+    componentWillUnmount() {
     }
 
     handleNewGame() {
+        document.getElementById('newGame').disabled = true
+        document.getElementById('check').disabled = false
         this.setState(
             (state, props) => {
                 return {
+                    info : props.info,
+                    attemptAmount : props.attemptAmount,
+                    result : props.result,
                     value: Math.floor(Math.random() * (1000 - 1 + 1)) + 1
                 };
             }
@@ -30,14 +49,45 @@ export class Number extends React.Component {
     handleCheck() {
         this.setState(
             (state, props) => {
-                if (document.getElementById('getNumber').value===this.state.value)
-                return {
-                    result : 'true'
-                };
-                else 
+                this.showInfo()
+                if (parseInt(document.getElementById('getNumber').value)===state.value
+                    && state.attemptAmount>0) {
+                    document.getElementById('newGame').disabled = false;
+                    document.getElementById('check').disabled = true;
                     return {
-                        result : 'nuh-uh'
+                        result : 'You win'
+                    };
+                }
+
+                if (parseInt(document.getElementById('getNumber').value)!==state.value
+                && state.attemptAmount===1) {
+                    document.getElementById('newGame').disabled = false;
+                    document.getElementById('check').disabled = true;
+                    return {
+                        result : 'Game over'
+                    };
+                }
+            }
+        )
+    }
+
+    showInfo() {
+        this.setState(
+            (state, props) => {
+                if (parseInt(document.getElementById('getNumber').value) < state.value
+                    && state.attemptAmount>0) {
+                    return {
+                        info: this.state.info + 'N > ' + document.getElementById('getNumber').value + ' ',
+                        attemptAmount : state.attemptAmount - 1,
                     }
+                }
+                if (parseInt(document.getElementById('getNumber').value) > state.value
+                    && state.attemptAmount>0) {
+                    return {
+                        info: this.state.info + 'N < ' + document.getElementById('getNumber').value.toString() + ' ',
+                        attemptAmount : state.attemptAmount - 1,
+                    }
+                }
             }
         )
     }
@@ -45,14 +95,14 @@ export class Number extends React.Component {
     render() {
         return (
             <div class="container">
-                <button type="button" onClick={this.handleNewGame}>New Game</button>
-                <input type="text" id="getNumber"/>
-                <button type="button" onClick={this.handleCheck}>Check</button>
-                <div>Information:</div>
-                <div>Random number {this.state.value}</div>
-                <div>Attempts:</div>
-                <div>Result:</div>
-                <div>{this.state.result}</div>
+                <button type="button" class="bp4-button bp4-outlined bp4-large" id='newGame' onClick={this.handleNewGame}>New Game</button>
+                <input type="number" id="getNumber" class="bp4-input"/>
+                <button type="button" class="bp4-button bp4-outlined bp4-large" id='check' onClick={this.handleCheck}>Check</button>
+                <div class="container_info">
+                    <div>Information: {this.state.info}</div>
+                    <div>Attempts: {this.state.attemptAmount}</div>
+                    <div>Result: {this.state.result}</div>
+                </div>
             </div>
         )
     }
